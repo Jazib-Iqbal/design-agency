dc9ba902d89c6ef0c5746b175fc9247d7872fdf7
Jazib Iqbal <you@example.comm.jazibiqbal@gmail.com>
2023-07-15 13:22:31 +0500

    no comment

---
+++ js/main.js
@@ -0,0 +1,16 @@
+let hamburger = document.querySelector("#hamburger");
+
+let nav_list = document.querySelector("#nav_list")
+
+let nav_sect = document.querySelector("#nav_sect")
+
+
+
+hamburger.addEventListener('click', function() {
+
+    nav_list.classList.toggle('nav_toggle')
+
+    // nav_sect.style.zIndex = "100" 
+  
+  })
+
